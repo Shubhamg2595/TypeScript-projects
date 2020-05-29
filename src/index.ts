42b799199f34cec442db09d05b048db5562ba1b1
Shubham Gupta <shubhamg2595@gmail.com>
2020-05-29 18:59:45 +0530

    company TS added

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hello typescript');
+import { User } from "./User";
+import { Company } from "./Company";
+
+const user = new User();
+const company = new Company();
+
+console.log("user -", user);
+console.log("company -", company);
