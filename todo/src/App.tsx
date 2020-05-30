eb3bbb12a3317e97ff94a9215ec32fee5b0c4c02
Shubham Gupta <shubhamg2595@gmail.com>
2020-05-30 15:22:24 +0530

    typeScript added with CRA

--- todo/src/App.tsx
+++ todo/src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { setupMaster } from "cluster";
 
 function App() {
+  const sum = (a: number, b: number): number => {
+    return a + b;
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +15,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {console.log('sum is',sum(2,4))}
         <a
           className="App-link"
           href="https://reactjs.org"
