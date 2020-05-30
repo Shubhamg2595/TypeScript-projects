8428f0b2c45e8c6368f7620490deef941384d9cf
Shubham Gupta <shubhamg2595@gmail.com>
2020-05-31 00:15:43 +0530

    creating and dispatching action to get episodesList

--- episode-picker/src/Components/Home.tsx
+++ episode-picker/src/Components/Home.tsx
@@ -1,12 +1,28 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { Store } from "../Store/store";
 export default function Home(): JSX.Element {
-  const store = useContext(Store);
+  const { state, dispatch } = useContext(Store);
+
+  useEffect(() => {
+    fetchDataAction();
+  }, []);
+
+  const fetchDataAction = async () => {
+    const url =
+      "https://api.tvmaze.com/singlesearch/shows?q=rick-&-morty&embed=episodes";
+    const data = await fetch(url);
+    const jsonData = await data.json();
+
+    return dispatch({
+      type: "FETCH_DATA",
+      payload: jsonData._embedded.episodes,
+    });
+  };
 
   return (
     <div>
+      {console.log(state)}
       <h2> Rick and Morty epsiode picker </h2>
-      {console.log(store)}
     </div>
   );
 }
