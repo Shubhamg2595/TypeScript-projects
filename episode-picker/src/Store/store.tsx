6cd4fa72c15e3288e049ea5dc69358d86c030d72
Shubham Gupta <shubhamg2595@gmail.com>
2020-05-30 23:55:00 +0530

    reducer added for data fetching and connected with context using useReducer hook

--- episode-picker/src/Store/store.tsx
+++ episode-picker/src/Store/store.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useReducer } from "react";
 
 // using contextAPI via useContext hook and useReducer hook while following Redux principles.
 
@@ -6,18 +6,38 @@ interface IState {
   episodes: [];
   favourites: [];
 }
+interface IAction {
+  type: string;
+  payload: any;
+}
 
 const initialState: IState = {
   episodes: [],
   favourites: [],
 };
 
-export const Store = React.createContext<IState>(initialState);
+export const Store = React.createContext<IState | any>(initialState);
 
-function reducer() {}
+function reducer(state: IState = initialState, action: IAction): IState {
+  switch (action.type) {
+    case "FETCH_DATA":
+      return {
+        ...state,
+        episodes: action.payload,
+      };
+
+    default:
+      return state;
+  }
+}
 
 export function StoreProvider(props: any): JSX.Element {
+  const [state, dispatch] = React.useReducer(reducer, initialState);
+
   return (
-    <Store.Provider value={initialState}> {props.children} </Store.Provider>
+    <Store.Provider value={{ state, dispatch }}>
+      {" "}
+      {props.children}{" "}
+    </Store.Provider>
   );
 }
