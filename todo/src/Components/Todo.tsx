57818cc82a6a0eef65c00a9df225377f5dee28cb
Shubham Gupta <shubhamg2595@gmail.com>
2020-05-30 16:10:04 +0530

    difference between todos and interfaces

--- todo/src/Components/Todo.tsx
+++ todo/src/Components/Todo.tsx
@@ -1,9 +1,26 @@
 import React, { useState } from "react";
 
+// ? difference between typs and interfaces
+
+// * types simply refers to another type that exists
 type FormElem = React.FormEvent<HTMLFormElement>;
+
+// * with interfaces we can create new types and also perforn inheritance
+
+interface ITodo {
+  text: string;
+  complete: boolean;
+}
+
+// inheritance with TS interfaces
+
+interface ITodo2 extends ITodo {
+  tags: string[];
+}
 
 export default function Todo() {
   const [value, setValue] = useState<string>("");
+  const [todos, setTodos] = useState<ITodo[]>([]);
 
   function handleSubmit(e: FormElem) {
     e.preventDefault();
